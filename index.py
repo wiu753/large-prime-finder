142cf64a25b718e4e12e032c94af6f264db7c429
Martin Wiulsrød <wiu753@gmail.com>
2022-11-19 12:10:37 +0100

    Add Miller Rabin and Solowoy Strassen algorithms and some driver code

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import random
 
-def generateOddNumber():
+def generate_odd_number():
     odd_number = random.randrange(1, 2**1024, 2)
     
     while (odd_number.bit_length() != 1024):
@@ -14,7 +14,63 @@ def isNumberOdd(number):
     else:
         return True
 
-x = generateOddNumber()
-print(x)
-print(x.bit_length())
-print(isNumberOdd(x))
+def miller_rabin(number, iterations):
+    if (number == 2):
+        return True
+    if (number == 1 or number % 2 == 0):
+        return False
+    
+    # Find r and s
+    r = 0
+    s = number - 1
+    while (s % 2 == 0):
+        r += 1
+        s //= 2
+    
+    for i in range(iterations):
+        a = random.randrange(2, number - 1)
+        x = pow(a, s, number)
+        if (x == 1 or x == number - 1):
+            continue
+        
+        for j in range(r - 1):
+            x = pow(x, 2, number)
+            if (x == number - 1):
+                break
+        else:
+            return False
+    
+    return True
+
+def solovoy_strassen(number, iterations):
+    if (number == 2):
+        return True
+    if (number == 1 or number % 2 == 0):
+        return False
+    
+    for i in range(iterations):
+        a = random.randrange(2, number - 1)
+        if (pow(a, (number - 1) // 2, number) == number - 1):
+            continue
+        
+        if (pow(a, (number - 1) // 2, number) != 1):
+            return False
+    
+    return True
+
+test = generate_odd_number()
+print(test)
+print(isNumberOdd(test))
+print(test.bit_length())
+
+for i in range(1000):
+    number = generate_odd_number()
+    if (miller_rabin(number, 100)):
+        print("Miller Rabin: Prime number is {}".format(number))
+        break
+
+for i in range(1000):
+    number = generate_odd_number()
+    if (solovoy_strassen(number, 100)):
+        print("Solowoy Strassen: Prime number is {}".format(number))
+        break
